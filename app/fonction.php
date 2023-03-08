89977cab644711b97fd707e6937f08f82a103587
NKP <nnkp066@gmail.com>
2023-03-08 19:40:11 +0100

    nouvelle branche

--- app/fonction.php
+++ app/fonction.php
@@ -295,3 +295,293 @@ function MiseAuPanier($user_id, $id_article)
 
 
   
+<?php
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Models\shop_article;
+use App\Models\shop_article_1;
+use App\Models\LiaisonShopArticlesBill;
+use App\Models\Shop_category;
+
+//fonction pour afficher la famille en fonction de l'id de la famille
+function getUsersByFamilyId($family_id)
+{
+    $users = User::where('family_id', $family_id)->get();
+    return $users;
+}
+
+//faire sortir la saison active en fonction de la table parametre
+function saison_active()
+{
+    $saison = DB::table('parametre')
+    ->where('activate', '=', 1)
+    ->value('saison');
+    return $saison;
+}
+
+//filtrer les articles en fonction de leur date de validité
+function filterArticlesByValidityDate($articles) {
+    $now = date('Y-m-d');
+
+    $filteredArticles = collect($articles)->filter(function($article) use($now) {
+        return ($article->startvalidity <= $now && $article->endvalidity >= $now);
+    })->values();
+
+    return $filteredArticles;
+}
+
+//filtrer les articles en fonction de selected_limit,Age & sexe
+function getFilteredArticles($articles) {
+    if (Auth::check()) {
+        $user = Auth::user();
+        $family = getUsersByFamilyId($user->family_id);
+        $familyGender = $family->pluck('gender')->unique();
+        $articleIds = collect($articles)->pluck('id_shop_article');
+    
+        $selectedArticles = DB::table('selected_limit')
+                                ->whereIn('id_shop_article', $articleIds)
+                                ->whereIn('user_id', $family->pluck('user_id'))
+                                ->pluck('id_shop_article')
+                                ->toArray();
+    
+        $filteredArticles = collect($articles)->filter(function($article) use ($selectedArticles, $user, $familyGender, $family) {
+            if ($article->sex_limit && !$familyGender->contains($article->sex_limit)) {
+                return false;
+            }
+    
+            if ($article->selected_limit == 0) {
+                return hasFamilyMemberWithAgeInRange($family, $article->agemin, $article->agemax) || in_array($article->id_shop_article, $selectedArticles);
+                
+            } else {
+                return in_array($article->id_shop_article, $selectedArticles);
+            }
+        })->values();
+    
+    } else {
+        $filteredArticles = collect($articles)->filter(function($article) {
+            return ($article->selected_limit == 0 );
+        })->values();
+    }
+    
+    return $filteredArticles;
+}
+
+//filtrer les articles en fonction de l'age de la famille
+function hasFamilyMemberWithAgeInRange($familyMembers, $agemin, $agemax) {
+    foreach ($familyMembers as $member) {
+        $age = Carbon::parse($member->birthdate)->age;
+        if ($age >= $agemin && $age <= $agemax) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//sortir les membres de la famille qui ont l'age requis
+function getFamilyMembersMeetingAgeCriteria($family, $agemin, $agemax) {
+    $members = collect($family)->filter(function($member) use ($agemin, $agemax) {
+        $age = Carbon::parse($member->birthdate)->age;
+        return ($age >= $agemin && $age <= $agemax);
+    })->values();
+    
+    return $members;
+}
+
+
+// sortir les users qui peuvent acheter  l'article
+function getArticleUsers($article) {
+    $selectedArticles = [];
+    $selectedUsers = [];
+
+        $user = Auth::user();
+        $family = getUsersByFamilyId($user->family_id);
+
+        $selectedArticles = DB::table('selected_limit')
+            ->where('id_shop_article', $article->id_shop_article)
+            ->whereIn('user_id', $family->pluck('user_id'))
+            ->pluck('id_shop_article')
+            ->toArray();
+
+        $selectedUsers = DB::table('selected_limit')
+            ->where('id_shop_article', $article->id_shop_article)
+            ->whereIn('user_id', $family->pluck('user_id'))
+            ->pluck('user_id')
+            ->toArray();
+
+    $filteredUsers = collect($family)->filter(function($user) use ($selectedUsers, $article) {
+        return in_array($user->user_id, $selectedUsers) && ($article->sex_limit == null || $user->gender == $article->sex_limit);
+    })->values();
+
+    $familyMembersMeetingAgeCriteria = getFamilyMembersMeetingAgeCriteria($family, $article->agemin, $article->agemax);
+    $familyMembersMeetingAgeCriteria = $familyMembersMeetingAgeCriteria->filter(function($user) use ($article) {
+        return ($article->sex_limit == null || $user->gender == $article->sex_limit);
+    })->values();
+
+    $filteredUsers = $filteredUsers->merge($familyMembersMeetingAgeCriteria);
+
+    return $filteredUsers;
+
+
+}
+
+// recuperer l'ID de user et la saison et restituer les produits dont il est teacher en fonction de la saison
+function retourner_shop_article_dun_teacher($user_id, $saison) {
+
+    $selectedArticles = [];
+    $selectedArticles_byuser = [];
+    //recupere le role du user 
+    $user_role = User::where("user_id",$user_id)->pluck('role')->first();
+   
+   
+    if ($user_role >= 30 and $user_role<90) {
+
+   
+        $shop_article_saison = Shop_article_1::get();
+
+        // recupere les shops article de type lesson
+        $shop_article_lesson =   DB::table('shop_article')->where('saison',$saison)
+       ->join('shop_article_1', 'shop_article_1.id_shop_article', '=', 'shop_article.id_shop_article')->get()->toArray();
+
+       foreach($shop_article_lesson as $data1){
+
+        $Data_teacher = json_decode($data1->teacher,true);
+
+            foreach($Data_teacher as $data2){
+                        if($user_id == $data2){
+                            $selectedArticles_byuser[] = $data1->id_shop_article;
+                        }
+            }
+
+       }
+
+       return  $selectedArticles_byuser ;
+
+
+        //recupere l'ensemble des teachers et l'id article(tableau with id)
+       $shop_article_l =   DB::table('shop_article')->where('saison',$saison)
+       ->join('shop_article_1', 'shop_article_1.id_shop_article', '=', 'shop_article.id_shop_article')->pluck('shop_article_1.id_shop_article','teacher')->toArray();
+      
+
+    }elseif($user_role>=90){
+        $shop_article = shop_article::where('saison',$saison)->get()->toArray();
+        $selectedArticles = $shop_article;
+        return $selectedArticles ;
+    }
+        
+    
+
+}
+
+
+// recuperer l'ID de user et la saison et restituer les shop articles achetes
+function Donne_articles_Paye_d_un_user_aucours_d_une_saison($user_id, $saison) {
+
+    
+    $selectedArticles_buy = [];
+  
+    $shop_article_with_status = DB::table('bills')->where('status','>',60)->Where('id_user',$user_id)
+    ->join('liaison_shop_articles_bills', 'liaison_shop_articles_bills.bill_id', '=', 'bills.id')->pluck('id_shop_article')->unique()->toArray();
+
+    $shop_article_saison_actuelle = DB::table('shop_article')->where('saison',$saison)->pluck('id_shop_article')->unique()->toArray() ;
+
+    foreach ($shop_article_with_status as $value) {
+
+       if(in_array($value,$shop_article_saison_actuelle)){
+        $selectedArticles_buy[] = $value ;
+       }
+
+
+    }
+
+
+    return $selectedArticles_buy;
+
+}
+
+// recuperer l'ID d'un shop articles et ramene tous les users qui l'ont achetees
+function Donne_User_article_Paye($id_shop_article) {
+
+    $shop_article_with_status = DB::table('bills')->where('status','>',60)->Where('id_shop_article',$id_shop_article)
+    ->join('liaison_shop_articles_bills', 'liaison_shop_articles_bills.bill_id', '=', 'bills.id')->pluck('id_user')->unique()->toArray();
+
+    return   $shop_article_with_status;
+
+}
+// recuperer l'ID d'un shop articles et ramene tous les users 
+function Donne_User_article($id_shop_article) {
+
+    $shop_article_with_status = DB::table('bills')->Where('id_shop_article',$id_shop_article)
+    ->join('liaison_shop_articles_bills', 'liaison_shop_articles_bills.bill_id', '=', 'bills.id')->pluck('id_user')->unique()->toArray();
+
+    return   $shop_article_with_status;
+
+}
+
+
+// recuperer l'ID d'un shop articles et retourne les USERS qui ont achete le produit a une date anterieure
+function Donne_User_article_Date($id_shop_article,$date1) {
+
+    $shop_article_with_date = DB::table('bills')->where('status','>',60)->Where('id_shop_article',$id_shop_article)->whereDate('date_bill', '<', $date1)
+    ->join('liaison_shop_articles_bills', 'liaison_shop_articles_bills.bill_id', '=', 'bills.id')->pluck('id_user')->unique()->toArray();
+
+    return  $shop_article_with_date;
+   
+  
+}
+
+
+
+function Inscrits_Saison_Date($sasion, $date1){
+
+}
+
+
+
+function Inscrits_Saison_Final($saison){
+
+}
+
+
+
+
+
+
+
+
+
+
+// recuperer l'ID d'un shop article et ramene tous ceux qui ont achete le produit (buyers)
+
+function retourner_buyers_dun_shop_article($id_shop_article) {
+
+    $requete_liaison_shop_article_bills = LiaisonShopArticlesBill::where('id_shop_article',$id_shop_article)->pluck('id_user')->toArray();
+
+    return $requete_liaison_shop_article_bills ;
+
+}
+
+
+
+
+//fonctions pour afficher les dates en Anglais
+function fetchDay($date){
+    $lejour = ( new DateTime($date) )->format('l');
+
+  $jour_semaine = array(
+"lundi" => "Monday",
+"Mardi" => "Tuesday",
+"Mercredi" => "Wednesday",
+"Jeudi" => "Thursday",
+"Vendredi" => "Friday",
+"Samedi" => "Saturday",
+"Dimanche" => "Sunday"
+
+  );
+
+}
+
+
+  
