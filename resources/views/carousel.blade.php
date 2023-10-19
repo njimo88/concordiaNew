71b3a832b8980f89e3b063c1fbc2f5a8119f6350
iamNedjmo <nedjmeddine.djilly@viacesi.fr>
2023-10-19 12:11:54 +0200

    fdsfs

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -146,30 +146,10 @@ img[alt="Logo"] {
     </div>
 @endif
 @if($errors->has('captcha'))
-    <!-- Modal -->
-    <div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Erreur</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    {{ $errors->first('captcha') }}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <script>
-        $(document).ready(function() {
-            $('#errorModal').modal('show');
-        });
+        alert('{{ $errors->first('captcha') }}');
     </script>
 @endif
-
 
 <section style="background-image: url('{{ $imageUrls[0] }}');" class="bg-light position-relative bg-cover top-banner">
     <img loading="lazy" src="{{ asset("/assets/images/gymm.jpg") }}" alt="Blog" title="Blog" class="d-none">
